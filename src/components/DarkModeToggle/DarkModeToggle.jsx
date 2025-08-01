9d93a83db421de8cf31aca47c74215088b1c2ad3
Akhil roczz <b.akhil667@gmail.com>
2025-08-01 22:21:05 +0530

    updated

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -9,7 +9,7 @@ const DarkModeToggle = () => {
 
 
   useEffect(()=>{
-    if(showdarkMode){
+    if(showdarkMode){  //true
         document.body.classList.add("dark-mode")
     }else{
         document.body.classList.remove("dark-mode");
